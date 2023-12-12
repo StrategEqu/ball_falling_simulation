65f866c319c94b9fb9679106cd19308fe89a684f
Дмитрий Когтев <129877863+StrategEqu@users.noreply.github.com>
2023-12-12 15:10:29 +0300

    Delete src/Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        BallMove ballMove = new BallMove();
-        ballMove.setHeightTown(100);
-        ballMove.calculation(ballMove.getHeightTown(), BallMove.GRAVITY);
-    }
-}
