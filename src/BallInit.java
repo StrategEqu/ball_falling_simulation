eac77851ff0b23035c9b9f2d219b4abda4de9ece
Дмитрий Когтев <129877863+StrategEqu@users.noreply.github.com>
2024-01-07 17:23:58 +0300

    Add files via upload

---
+++ src/BallInit.java
@@ -0,0 +1,12 @@
+public abstract class BallInit {
+    public static final double GRAVITY = 9.8;
+    private int heightTown;
+
+    public int getHeightTown() {
+        return heightTown;
+    }
+
+    public void setHeightTown(int height) {
+        this.heightTown = height;
+    }
+}
